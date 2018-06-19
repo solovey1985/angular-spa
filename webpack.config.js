0e1a38a43038bc143efab7a69e52bae4922680a0
SOFTINVEST\Andrii.Solovienko <andrii.solovienko@uapay.ua>
2018-06-19 15:29:54 +0300

    webpack development mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: "development",
   entry: { 'boot-server': './ClientApp/boot-server.ts' },
   resolve: { 
     extensions: [ '.js', '.ts' ]
